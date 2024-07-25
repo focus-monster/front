f653a2b2801fc5ac7da9e4a5b0751bfd7d0c8f49
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-07-26 01:47:24 +0900

    feat: add signin and other links

--- src/components/folder/index.tsx
+++ src/components/folder/index.tsx
@@ -2,21 +2,31 @@ import { PropsWithChildren, ReactNode } from "react";
 
 type Folder = {
   insert?: ReactNode;
+  className?: string;
 };
 
 export default function Folder({
+  className,
   children,
   insert,
 }: PropsWithChildren<Folder>) {
   return (
-    <div className="w-lg relative pl-16">
-      <img className="w-full" src="/folder-back.png" />
-      {insert ? <div className="absolute right-32 top-0">{insert}</div> : null}
+    <div className="relative">
+      <img className="w-[34rem]" src="/folder-back.png" />
+      {insert ? (
+        <div className="absolute right-[4rem] top-0">{insert}</div>
+      ) : null}
       <img
-        className="absolute bottom-0 right-0 w-full"
+        className="absolute bottom-0 right-0 w-[37rem] max-w-none"
         src="/folder-front.png"
       />
-      <div className="isolate z-10">{children}</div>
+      <div
+        className={
+          "absolute inset-6 isolate z-10 grid items-end justify-center"
+        }
+      >
+        <div className={className}>{children}</div>
+      </div>
     </div>
   );
 }
