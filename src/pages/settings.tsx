4800f1dbcb92dd456f9c2ec5a6264944bee5b76d
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-08-12 00:53:25 +0900

    feat: settings qa

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 import { Job } from "./onboarding";
+import { cn } from "@/lib/utils";
 
 export default function Settings() {
   console.log("hi?");
@@ -26,8 +27,10 @@ function BannedSites() {
 
   return (
     <div className="flex grow flex-col space-y-4 pb-6 lg:pb-0 lg:pr-6">
-      <h2 className="text-2xl font-semibold">Blocked Websites</h2>
-      <p className="text-gray-800">
+      <h2 className="text-2xl font-semibold text-neutral-100">
+        Blocked Websites
+      </h2>
+      <p className="text-neutral-100">
         Don't open the websites, let your FocusMonster grow!
       </p>
       <div>
@@ -123,8 +126,9 @@ function UserProfile() {
 
   const [nickname, setNickname] = useState(data?.nickname);
   const [job, setJob] = useState(data?.job ?? "");
+  const [nicknameError, setNicknameError] = useState("");
 
-  const { mutate } = useMutation({
+  const { mutate, isSuccess } = useMutation({
     mutationKey: ["update-profile"],
     mutationFn: async () => {
       const res = await fetch("/api/users/onboarding", {
@@ -144,18 +148,25 @@ function UserProfile() {
       return await res.json();
     },
     onSuccess: () => {
-      toast.success("Profile updated");
+      toast.success("Profile updated successfully!");
     },
     onError: (error) => {
-      toast.error(error.message);
+      if (error.message.includes("Invalid Nickname")) {
+        setNicknameError("The nickname is already taken");
+      }
     },
   });
 
   return (
     <div className="flex grow flex-col space-y-4 pl-6 pt-0">
-      <h2 className="text-2xl font-semibold">User Profile</h2>
-      <div className="grid grid-cols-[200px__1fr] place-content-center items-center gap-2">
-        <div className="">Nickname</div>
+      <h2 className="text-2xl font-semibold text-neutral-100">User Profile</h2>
+      <div className="relative grid grid-cols-[200px__1fr] place-content-center items-center gap-2 pt-10">
+        {nicknameError.length > 0 && (
+          <div className="absolute -top-1 rounded-full bg-red-100 px-4 py-1 text-red-600">
+            {nicknameError}
+          </div>
+        )}
+        <div className="text-neutral-100">Nickname</div>
         <Button
           variant="outline"
           className="bg-transparent hover:bg-neutral-50/20"
@@ -166,28 +177,43 @@ function UserProfile() {
             className="rounded-lg bg-transparent p-2 text-neutral-200"
             value={nickname}
             onChange={(e) => {
-              if (e.target.value.length > 13) {
-                toast.error("Nickname should be less than 13 characters");
-                return;
-              }
-              if (e.target.value.length > 0 && !regex.test(e.target.value)) {
-                toast.error(
-                  "Nickname should be alphanumeric. No special characters",
+              if (
+                e.target.value.length > 13 ||
+                (e.target.value.length > 0 && !regex.test(e.target.value))
+              ) {
+                setNicknameError(
+                  "*Nickname should be in English, Korean, or numbers, up to 13 characters.",
                 );
                 return;
               }
+              setNicknameError("");
               setNickname(e.target.value);
             }}
             placeholder={data?.nickname}
           />
         </Button>
-        <div className="">Job</div>
+        <div className="text-neutral-100">Job</div>
         <Job myJob={job} setMyJob={setJob} transparent />
-        <div className="">Language</div>
+        <div className="text-neutral-100">Language</div>
         <p className="rounded-lg p-2 text-gray-400">English</p>
       </div>
-      <Button onClick={() => mutate()} className="self-end">
-        Update
+      <Button
+        onClick={() => {
+          if (nicknameError.length > 0) {
+            return;
+          }
+          if (job.length < 1) {
+            toast.error("Please select your job");
+            return;
+          }
+          mutate();
+        }}
+        className={cn(
+          "self-end",
+          isSuccess && "bg-green-100 text-green-900 hover:bg-green-300",
+        )}
+      >
+        Update{isSuccess ? "d!" : null}
       </Button>
     </div>
   );
