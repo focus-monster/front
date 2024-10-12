3e58349243dd1ed4ca7ef9587afbbbd82a9b5085
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-10-12 22:09:07 +0900

    feat: add better error boundary

--- src/error-boundary.tsx
+++ src/error-boundary.tsx
@@ -1,8 +1,21 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, ErrorResponse } from "react-router-dom";
 
 export function ErrorBoundary() {
-  const error = useRouteError();
+  const error = useRouteError() as ErrorResponse;
   console.error(error);
   // Uncaught ReferenceError: path is not defined
-  return <div>Dang!</div>;
+  return (
+    <div className="grid h-screen w-full place-content-center place-items-center text-white">
+      <h1 className="pb-12 text-center text-4xl">
+        Something unexpected happened...
+      </h1>
+      <p className="pb-2 text-lg">Here is the reason why it errored</p>
+      <div className="rounded-lg bg-red-200 p-4 text-center text-lg text-red-700">
+        <pre>
+          [{error.status}] {error.statusText}
+        </pre>
+        <pre>{error.data}</pre>
+      </div>
+    </div>
+  );
 }
