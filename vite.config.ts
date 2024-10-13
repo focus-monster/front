a224433dcb317328ba2915516acc84c5424f7cff
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-10-13 12:08:29 +0900

    fix: change port

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export const API_URL = "https://focusmonster.me:8080";
+export const API_URL = "https://focusmonster.me:8088";
 export const GEMINI_URL = "http://focusmonster.me:8090";
 
 // https://vitejs.dev/config/
