80ea37bf7aab8ea87c1fd6d0229d73ee204c623a
이채준 - CJ Lee <chaejunlee@knu.ac.kr>
2025-06-22 16:32:50 +0900

    hotfix: give absolute path to podman

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ cd ~/fe/front
 git fetch origin main
 git reset --hard origin/main
 
-podman build -t docker.io/chaejunlee/focus-monster-fe .
+/opt/homebrew/bin/podman build -t docker.io/chaejunlee/focus-monster-fe .
 
-podman rm -f focus-monster-fe
+/opt/homebrew/bin/podman rm -f focus-monster-fe
 
-podman run -d -p 4173:4173 --restart unless-stopped --name focus-monster-fe docker.io/chaejunlee/focus-monster-fe
+/opt/homebrew/bin/podman run -d -p 4173:4173 --restart unless-stopped --name focus-monster-fe docker.io/chaejunlee/focus-monster-fe
